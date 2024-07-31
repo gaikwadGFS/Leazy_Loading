1cddddd77341902fb0902b09b3ec24b0443c7b21
gaikwadGFS <kshitija.gaikwad@growingflowers-solutions.com>
2024-07-31 10:58:04 +0530

    aftercontentcheck & afterviewInit

--- src/app/Components/demo/demo.component.ts
+++ src/app/Components/demo/demo.component.ts
@@ -1,18 +1,23 @@
-import { Component, ContentChild, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.css'
 })
-export class DemoComponent {
+export class DemoComponent implements AfterViewInit {
 
   inputValue:string='';
 
  
 
   constructor(){
-    console.log("Demo Parent Componet called");
+    // console.log("Demo Parent Componet called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit Hooks called");
+    
   }
   save(txt:HTMLInputElement){
    this.inputValue=txt.value;
--- src/app/Components/ng-onchanges/ng-onchanges.component.ts
+++ src/app/Components/ng-onchanges/ng-onchanges.component.ts
@@ -1,40 +1,55 @@
-import { AfterContentInit, Component,  ContentChild,  ContentChildren,  DoCheck,  ElementRef,  Input, OnChanges, OnInit, QueryList, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component,
+    ContentChild,  ContentChildren,  DoCheck,  ElementRef,  
+    Input, OnChanges, OnInit, QueryList, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ng-onchanges',
   templateUrl: './ng-onchanges.component.html',
   styleUrl: './ng-onchanges.component.css'
 })
-export class NgOnchangesComponent implements OnChanges,OnInit,DoCheck,AfterContentInit {
+export class NgOnchangesComponent implements OnChanges,OnInit,DoCheck,
+                                            AfterContentInit,AfterContentChecked,
+                                            AfterViewInit {
 
   @Input() msg:string='';
 
   @ContentChild('para') paraRef!:ElementRef;
   @ContentChildren('para') paraMultiRef!:QueryList<ElementRef>;
 
+  @ViewChild('temp') tempRef!:ElementRef;
+
+
   constructor(){
-    console.log("ng on changes component parent Called")
+    // console.log("ng on changes component parent Called")
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("ngOnChanges Hooks Called");
-    console.log(changes);
+    // console.log("ngOnChanges Hooks Called");
+    // console.log(changes);
   }
 
   ngOnInit(): void {
-    console.log("NgonInit Hooks called");
+    // console.log("NgonInit Hooks called");
   }
 
   ngDoCheck(): void {
     console.log("ngDoCheck Hooks called");
-    console.log("do check:" , this.paraRef);
+    // console.log("do check:" , this.paraRef);
   }
   ngAfterContentInit(): void {
     console.log("ngAfterContentInit Hooks called");
     console.log(":After content Init" , this.paraRef.nativeElement);
-    console.log(this.paraMultiRef);
+    // console.log(this.paraMultiRef);
   }
 
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked Hooks called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit Hooks called");
+    console.log("In ngAfterViewInit" ,this.tempRef.nativeElement)
+  }
  
 }
 
